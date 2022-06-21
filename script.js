7c2895776c1400f7b099bb25bd308b99cdf42943
amaury <tissot.amaury@gmail.com>
2022-06-21 20:20:39 +0200

    Refractor JS with factory pattern

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-// Function who display "X" inside container grid
-const gridElement = document.querySelectorAll('.grid-display');
+const GameBoard = () => {
 
-const displayPlayerMove = () => {
-    gridElement.forEach(element => {
-        element.addEventListener('click', () => {
-            element.textContent = 'X';
+    // Grid array
+    let grid = ['0','1','2',
+    '3','4','5',
+    '6','7','8'];
+
+    // Var of the ID grid clicked
+    let idInfo;
+    
+    // Variable who select grid element
+    const gridElement = document.querySelectorAll('.grid-display');
+    
+    // Function who reset the grid on click
+    const _resetGrid = () => {
+        gridElement.forEach(element => {
+            element.textContent = '';
         })
-    })
-}
+    };
+    
+    // Function who activate _resetGrid() on click
+    const activateReset = () => {
+        const buttonElement = document.querySelector ('.reset-button');
+        buttonElement.addEventListener('click', () => {
+            _resetGrid();
+        })
+    };
 
-displayPlayerMove();
+    // Function who display "X" inside a container grid and get his ID;
+    const displayPlayerMove = () => {
+        gridElement.forEach(element => {
+            element.addEventListener('click', (event) => {
+                element.textContent = 'X';
+                idInfo = event.target.id;
+                console.log(idInfo);
+                _populateGridArray();
+                console.table(grid)
+            })
+        })
+    };
+    
+    // Function who "populate" the grid array
+    const _populateGridArray = () => {
+        grid.splice(idInfo,1,'X')
+    }
 
-// Function who "reset" the grid
+    return {grid, activateReset, displayPlayerMove}    
+};
 
-const resetGrid = () => {
-    gridElement.forEach(element => {
-        element.textContent = '';
-    })
-}
 
-// Function who activate resetGrid() on click
-
-const activateReset = () => {
-    const buttonElement = document.querySelector ('.reset-button');
-    buttonElement.addEventListener('click', () => {
-        resetGrid();
-    })
-}
-
-activateReset();
-
+const test = GameBoard();
+test.displayPlayerMove();
